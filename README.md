487c492fa13d2ceb263d03d64659ca1047345ad5
Chanyoung Ahn <ahnchan@gmail.com>
2021-12-07 13:57:42 +0900

    changed s tutorial document url

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tutorial-spring-graphql
+# Spring GraphQL Tutorial
+
+Tutorial: [https://ahnchan.github.io/posts/GraphQL-Spring-GraphQL_M/](https://ahnchan.github.io/posts/GraphQL-Spring-GraphQL_M/) 
--- complete/graphql/src/main/java/com/ahnchan/graphql/GraphqlApplication.java
+++ complete/graphql/src/main/java/com/ahnchan/graphql/GraphqlApplication.java
@@ -1,12 +1,8 @@
 package com.ahnchan.graphql;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import graphql.com.google.common.base.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Example;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -15,9 +11,6 @@ import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class GraphqlApplication {
@@ -82,24 +75,41 @@ class ProductGraphqlController {
 		return this.reviewRepository.findByProductid(product.id());
 	}
 
+	/**
+	 * add a product
+	 * @param title Title
+	 * @param author Author
+	 * @param ISBN	ISBN
+	 * @return	return a added product information
+	 */
 	@MutationMapping
 	Mono<Product> addProduct(@Argument String title, @Argument String author, @Argument String ISBN) {
 		return this.productRepository.save(new Product(null, title, author, ISBN));
 	}
-
 }
 
+/**
+ * Product Repository
+ */
 interface ProductRepository  extends ReactiveCrudRepository<Product, Integer> {
 	Flux<Product> findByTitle(String title);
 }
 
+/**
+ * Review Repository
+ */
 interface ReviewRepository extends ReactiveCrudRepository<Review, Integer> {
 	Flux<Review> findByProductid(Integer productid);
 }
 
+/**
+ * Product Entity
+ */
 record Product(@Id Integer id, String title, String author, String ISBN) {
 }
 
+/**
+ * Review Entity
+ */
 record Review(@Id Integer id, Integer productid, String reviewer, String text) {
 }
-
